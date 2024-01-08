e1607f1dbe50c5a3a4f263fb3851d4fe221a8fc8
jakke <itsjakk3@gmail.com>
2024-01-08 14:50:24 +0900

    Make temporary item on Canvas

---
+++ 03_MakeTransient/src/MakeTransient/Command.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeTransient
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Default var
+            var uiapp = commandData.Application;
+            var app = uiapp.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            using (var graphicsManager = TemporaryGraphicsManager.GetTemporaryGraphicsManager(doc))
+            {
+                var insertPoint = uidoc.Selection.PickPoint("Select insertion point");
+                var imgData = new InCanvasControlData(@"C:\plus.bmp", XYZ.Zero);                    //You should update the path as yours
+                var index = graphicsManager.AddControl(imgData, ElementId.InvalidElementId);
+            }
+
+            //Termination
+            return Result.Succeeded;
+        }
+    }
+}
